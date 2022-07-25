ea609df6f93961bec47cfb9cd9838e1dfb8df3cf
AndreyIvchenko <6491666@gmail.com>
2022-07-25 20:51:47 +0300

    employees_sampl step 1

--- employees/src/components/app/app.js
+++ employees/src/components/app/app.js
@@ -1,10 +1,22 @@
-
+import AppInfo from '../app-info/app-info';
+import SearhPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
 
 import './app.css';
 
 function App() {
     return (
         <div className="app">
+            <AppInfo/>
+
+            <div className="search-panel">
+                <SearhPanel/>
+                <AppFilter/>
+            </div>
+
+            <EmployeesList/>
+
 
         </div>
     );
