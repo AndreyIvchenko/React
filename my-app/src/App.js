fa61f1e6e660ca1f9a3ae8314fd8350a14eba41d
AndreyIvchenko <6491666@gmail.com>
2022-07-26 21:56:51 +0300

    state set_state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,48 @@
-import {Component, StrictMode} from 'react';
+import {Component} from 'react';
+
+
 import './App.css';
 
-const Header = () => {
-	return <h2>Hello React</h2>
+
+class WhoAmI extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			years: 40,
+			text: '++++'
+		}
+	}
+
+	nextYear = () => {
+		//this.state.years++; - так мы изменить напрямую не МОЖЕМ! 
+		this.setState(state => ({
+			years: state.years + 1
+		}))
+	}
+
+	render() {
+		const {name, surname, link} = this.props;
+		return (
+			<div>
+				<button onClick={this.nextYear}>{this.state.text}</button>
+				<h1>My name is {name}, surname - {surname}, age: {this.state.years} </h1>
+				<a href={link}>My profile</a>
+			</div>
+		)
+	}
 }
+
+function App() {
+	return (
+	  <div className="App">
+			<WhoAmI name="Anna" surname="IV" link="#"/>
+			<WhoAmI name="Andr" surname="Ivch" link="#"/>
+
+	  </div>
+	);
+  }
+  
+  export default App;
 
 // const Field = () => {
 // 	const holder = 'Enter here';
@@ -16,38 +55,23 @@ const Header = () => {
 // 			style={styledField}/>
 // }
 
-class Field extends Component {
-	render() {
-		const holder = 'Enter here';
-		const styledField = {
-			width: '400px'
-		};
-		return <input 
-				placeholder={holder}
-				type ="text"
-				style={styledField}/>
-		}
-}
+// class Field extends Component {
+// 	render() {
+// 		const holder = 'Enter here';
+// 		const styledField = {
+// 			width: '400px'
+// 		};
+// 		return <input 
+// 				placeholder={holder}
+// 				type ="text"
+// 				style={styledField}/>
+// 		}
+// }
 
-function Btn() {
-	const text = 'Log in'
-	const logged = false;
-	return <button>{logged ? 'Enter' : text}</button> 
-}
+// function Btn() {
+// 	const text = 'Log in'
+// 	const logged = false;
+// 	return <button>{logged ? 'Enter' : text}</button> 
+// }
 
-
-function App() {
-  return (
-    <div className="App">
-		<StrictMode>
-			<Header/>
-		</StrictMode>
-		<Field/>
-		<Btn/>
-    </div>
-  );
-}
-
-
-export {Header};
-export default App;
+// export {Header};
