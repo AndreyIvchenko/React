77bef574f8c0e1fafe3d8035a3036179bc3f5411
AndreyIvchenko <6491666@gmail.com>
2022-07-27 21:45:46 +0300

    search and state lifting

--- employees/src/components/app/app.js
+++ employees/src/components/app/app.js
@@ -16,7 +16,8 @@ class App extends Component {
                 {name:"Anna I.", salary: 800, increase: false, rise:false, id:1},
                 {name:"Denis S.", salary: 3000, increase: false, rise:false, id:2},
                 {name:"Sasha G.", salary: 5000, increase: false, rise:false, id:3},
-            ]
+            ],
+            term: ''
         }
         this.maxId = 4;
     }
@@ -85,23 +86,37 @@ class App extends Component {
         }))
     }
    
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
 
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1 //indexOf return -1;
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
 
 
     render() {
+        const {data, term} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.searchEmp(data, term);
         return (
             <div className="app">
                 <AppInfo employees={employees} increased={increased}/>
     
                 <div className="search-panel">
-                    <SearhPanel/>
+                    <SearhPanel onUpdateSearch={this.onUpdateSearch}/>
                     <AppFilter/>
                 </div>
     
                 <EmployeesList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}
                     // onToggleIncrease={this.onToggleIncrease}
